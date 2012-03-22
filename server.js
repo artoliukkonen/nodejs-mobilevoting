3bdc45a2dc80e14b7170c20f80ccdcbb77056b44
Arto Liukkonen <arto.liukkonen@kolmaspiste.net>
2012-03-22 20:07:59 +0200

    Update

--- server.js
+++ server.js
@@ -35,6 +35,27 @@ app.get('/create', function(req, res){
   });
 });
 
+app.get('/quiz/save', function(req, res){
+  console.log('Loaded /quiz/save');
+  console.log(res);
+  console.log(req);
+  /*
+    var question = Question.build({
+      message: msg,
+      group_uuid: group_uuid,
+      UserId: user.id,
+    });
+    message.save().success(function(p) {
+      console.log('MESSAGE insert success');
+      
+      nowjs.getGroup(group_uuid).now.receiveMessage(message.message, user.user_uuid, user.name, message.message_id);
+    }).error(function(error) {
+      console.log('Insert failed :( ');
+      console.log(error);
+    });
+  */
+});
+
 // Database connection
 var Sequelize = require("sequelize");
 var sequelize = new Sequelize('votetime', 'votetime', null, {
@@ -49,19 +70,16 @@ var User = sequelize.define('User', {
   email: Sequelize.STRING
 });
 
-
-
 var Question = sequelize.define('Question', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-  text: Sequelize.STRING,
+  question: Sequelize.STRING,
   user_id: Sequelize.INTEGER
 });
 
 var Option = sequelize.define('Option', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   question_id: Sequelize.INTEGER,
-  text: Sequelize.STRING,
-  
+  value: Sequelize.STRING
 });
 
 var Answer = sequelize.define('Answer', {
@@ -80,12 +98,59 @@ Option.sync({force: true});
 Answer.sync({force: true});
 
 
+var nowjs = require("now");
+var everyone = nowjs.initialize(app);
 
+ nowjs.on('connect', function(){
+      console.log('User '+this.user.clientId+' connected');
+      
+      
+ });
+ 
+ nowjs.on('disconnect', function(){
+    console.log('User '+this.user.clientId+' disconnected');
+ });
+ 
+ 
+everyone.now.createQuestion = function(data) {
+
+  console.log(data);  
+  
+  var question = Question.build({
+    question: data[0].value,
+    user_id: 0 // TODO
+  });
+  
+  question.save().success(function() {
+    console.log('Question insert success');
+    console.log(this);
+    
+    for(i = 1; i<data.length; i++) {
+      var option = Option.build({
+        value: data[i].value,
+        question_id: this.id
+      });
+      
+      option.save().success(function() { 
+        console.log('Option insert success');
+      }).error(function(error) {
+        console.log('Option insert failed :( ');
+        console.log(error);
+      });
+    }
+  }).error(function(error) {
+    console.log('Question insert failed :( ');
+    console.log(error);
+  });
+  
+
+  
+};
 
 
 /* Helpers */
 
-var UUIDv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)}
+var UUIDv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)};
 
 var isMobile = function(req) {
   var ua = req.headers['user-agent'];
@@ -95,4 +160,4 @@ var isMobile = function(req) {
     $.Mobile = true;
       
   return $.Mobile;
-}
+};
